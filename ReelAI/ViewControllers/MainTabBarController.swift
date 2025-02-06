64ae813276a90f4d61937c65eb8bf58f851b2920
GauntletAI <gauntletai@Johns-MBP.attlocal.net>
2025-02-06 09:25:48 -0600

    pre livestream 2

--- ReelAI/ViewControllers/MainTabBarController.swift
+++ ReelAI/ViewControllers/MainTabBarController.swift
@@ -24,7 +24,7 @@ class MainTabBarController: UIViewController {
         homeVC.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"));
         homeVC.tabBarItem.tag = 0;
         
-        let friendsVC = UINavigationController(rootViewController: ProfileListsViewController())
+        let friendsVC = UINavigationController(rootViewController: LivestreamViewController())
         friendsVC.tabBarItem = UITabBarItem(title: "Friends", image: UIImage(systemName: "person.2"), selectedImage: UIImage(systemName: "person.2.fill"))
         friendsVC.tabBarItem.tag = 1;
         
